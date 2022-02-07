6d1c5044e8992b61116a87d601dbab3131b77eb2
humbleGuy1 <83718033+humbleGuy1@users.noreply.github.com>
2022-02-07 18:00:24 +0300

    Commit4

--- Adventure/Assets/Scripts/Spikes/Spikes.cs
+++ Adventure/Assets/Scripts/Spikes/Spikes.cs
@@ -6,6 +6,13 @@ public class Spikes : MonoBehaviour
 {
     [SerializeField] private int _damage;
 
+    private float _elapsedTime;
+
+    private void Start()
+    {
+        _elapsedTime = 0;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.TryGetComponent(out Player player))
@@ -13,4 +20,19 @@ public class Spikes : MonoBehaviour
             player.TakeDamage(_damage);
         }
     }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Player player))
+        {
+            _elapsedTime += Time.deltaTime;
+            
+            if(_elapsedTime > 1f)
+            {
+                player.TakeDamage(_damage);
+                _elapsedTime = 0;
+            }
+        }
+    }
+
 }
